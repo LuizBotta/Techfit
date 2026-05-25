5951c415229d686d8aa06adfe64a70b8a9189dfa
LuizBotta <luizbottaa@gmail.com>
2026-05-24 21:37:50 -0300

    Implementação da tela inicial

--- index.php
+++ index.php
@@ -3,9 +3,31 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>Tela Inicial</title>
 </head>
 <body>
+    <header> 
+        
+        <section class="logo">
+            <img src="assets/img/logo.png" alt="Imagem Da Logo">
+        </section>
+
+        <section class="menu-horizontal">
+            <ul>
+                <li><a href="index.php">INICIO</a></li>
+                <li><a href="">EXERCICIOS</a></li>
+                <li><a href="">TREINOS</a></li>
+                <li><a href="view/nutricao.php">NUTRIÇÃO</a></li>
+                <li><a href="view/login.php">SOBRE NÓS</a></li>
+            </ul>
+        </section>
+        
+        <section class="botao-entrar">
+            <button>ENTRAR</button> 
+        </section>
+
+    </header>
     
 </body>
 </html>
