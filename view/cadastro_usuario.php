f828f2b72cbda4253a8e3c4c5b1d456a643f4797
pabloferreirafatec-gif <pablo.ferreirafatec@gmail.com>
2026-05-26 23:30:17 -0300

    Implementação da tela de login

---
+++ view/cadastro_usuario.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../assets/css/login.css">
+    <title>Login</title>
+</head>
+<body>
+    <header> 
+            
+            <section class="logo">
+                <img src="../assets/img/logo.png" alt="Imagem Da Logo">
+            </section>
+    
+            <section class="menu-horizontal">
+                <ul>
+                    <li><a href="index.php">INICIO</a></li>
+                    <li class="dropdown">
+                        <a href="">EXERCICIOS</a>
+                    <ul class="submenu">
+                        <li><a href="">Academia</a></li>
+                        <li><a href="">Peso Corporal</a></li>
+                    </ul>
+                    </li>
+                    <li><a href="">TREINOS</a></li>
+                    <li><a href="view/nutricao.php">NUTRIÇÃO</a></li>
+                    <li><a href="view/login.php">SOBRE NÓS</a></li>
+                </ul>
+            </section>
+            
+            <section class="botao-entrar">
+                <button>ENTRAR</button> 
+            </section>
+    
+    </header>
+    
+    <section class="pagina-fundo">
+        <section class="fundo-forms">   
+            <section class="formulario">
+                <h2>Cadastrar Cliente</h2>
+                <form>
+                    <input type="text" placeholder="Nome" name="nome">
+                    <input type="text" placeholder="Sobrenome" name="sobrenome">
+                    <input type="text" placeholder="CPF" name="cpf">
+                    <input type="date" name="dataNasc">
+                    <input type="tel" placeholder="Telefone" name="telefone">
+                    <input type="email" placeholder="E-mail" name="email">
+                    <input type="password" placeholder="Senha" name="senha">
+                </form>
+                <section class="arquivo">
+                    <form >
+                        <label for="file-upload" class="custom-file-upload">Escolher Arquivo</label>
+                        <input id="file-upload" type="file" style="display:none;" name="arquivo"/>
+                    </form>
+                    <section id="file-name">Nenhum arquivo escolhido</section>
+                </section>
+                <section class="cadastrar">
+                    <p>CADASTRAR</p>
+                </section>
+                </section>
+            </section>
+        </section>
+    </section>
+    
+    
+    
+    <footer>
+    
+            <section class="footer-links">
+                <section class="navegacao">
+                    <h3>NAVEGAÇÃO</h3>
+                    <a href="#">Início</a>
+                    <a href="#">Exercícios</a>
+                    <a href="#">Treinos</a>
+                    <a href="#">Nutrição</a>
+                </section>
+
+                <section class="modalidade">
+                    <h3>MODALIDADES</h3>
+                    <a href="#">Peso Corporal</a>
+                    <a href="#">Musculação</a>
+                    <a href="#">Mobilidade</a>
+                    <a href="#">Alongamento</a>
+                </section>
+
+                <section class="contato">
+                    <h3>CONTATO</h3>
+                    <p>techfit@email.com.br</p>
+                    <p>Presidente Prudente - SP</p>
+                    <p>(18) 99799-9999</p>
+                </section>
+                
+                <section class="redes-sociais">
+                    <h3>REDES SOCIAIS</h3>
+                    
+                    <section class="item">
+                        <img src="assets/img/facebook.png">
+                        <p>Facebook</p>
+                    </section>
+                    
+                    <section class="item">
+                        <img src="assets/img/instagram.png">
+                        <p>Instagram</p>
+                    </section>
+                    
+                    <section class="item">
+                        <img src="assets/img/twitter.png">
+                        <p>Twitter</p>
+                    </section>
+                </section>
+            </section>
+
+            <section class="copyright">
+                <img src="../assets/img/logo.png" alt="logo techfit">
+                <p>© 2026 TECHFIT - Todos os direitos reservados.</p>
+            </section>
+    </footer>
+</body>
+</html>
